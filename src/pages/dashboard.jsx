2cf7bdf17c532c5fae4129af0ac600668698c7be
Zones Solution <dev.muhammadsaif@gmail.com>
2024-04-28 14:29:34 +0500

    updates

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -10,19 +10,19 @@ const Dashboard = () => {
             <div className='bg-white flex justify-center items-center py-24'>
                 <img src={car} alt="car" />
             </div>
-            <button className='bg-[#011E33] font-bold w-full py-8 text-[#bdbdbd] text-4xl'>Click Here To Chat With Us</button>
+            <button className='bg-[#011E33] font-bold w-full py-8 text-[#bdbdbd] text-4xl max-sm:text-xl'>Click Here To Chat With Us</button>
             <div className='flex flex-col py-8'>
                 <h2 className='text-[#011E33] text-3xl font-bold pb-4 text-center'>Shop For Parts</h2>
-                <div className='flex flex-col justify-center items-center gap-4'>
-                    <div className='flex gap-6'>
-                        <img src={partner1} alt="partner" />
-                        <img src={partner2} alt="partner" />
-                        <img src={partner3} alt="partner" />
+                <div className='flex flex-col justify-center items-center gap-4 max-sm:px-6'>
+                    <div className='flex gap-6 max-sm:flex-col'>
+                        <img className='max-sm:w-80' src={partner1} alt="partner" />
+                        <img className='max-sm:w-80' src={partner2} alt="partner" />
+                        <img className='max-sm:w-80' src={partner3} alt="partner" />
                     </div>
-                    <div className='flex gap-10'>
-                        <img src={partner4} alt="partner" />
-                        <img src={partner5} alt="partner" />
-                        <img src={partner6} alt="partner" />
+                    <div className='flex gap-10 max-sm:flex-col'>
+                        <img className='max-sm:w-80' src={partner4} alt="partner" />
+                        <img className='max-sm:w-80' src={partner5} alt="partner" />
+                        <img className='max-sm:w-80' src={partner6} alt="partner" />
                     </div>
                 </div>
             </div>
